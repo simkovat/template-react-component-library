d261f8ca54b3aa13df48c916a3e5d672d71c9632
Tereza Simkova <tereza.simkova@ext-livesport.eu>
2023-01-27 13:59:48 +0000

    Include css

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
 import packageJson from './package.json' assert { type: 'json' }
+import postcss from 'rollup-plugin-postcss'
 
 export default [
   {
@@ -22,12 +23,14 @@ export default [
     plugins: [
       resolve(),
       commonjs(),
-      typescript({ tsconfig: './tsconfig.json' })
+      typescript({ tsconfig: './tsconfig.json' }),
+      postcss()
     ]
   },
   {
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [dts()]
+    plugins: [dts()],
+    external: [/\.css$/] // exclude css from type exports
   }
 ]
